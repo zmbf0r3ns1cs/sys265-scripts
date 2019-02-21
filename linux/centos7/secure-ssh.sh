da92b441431869b3508a1d68d66acdfd87ca462a
zmbf0r3ns1cs <zburnham781@gmail.com>
2019-02-21 10:56:10 -0500

    Organized Directories

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,7 @@
+# SECURE-SSH.SH
+# AUTHOR: ZMBF0R3NS1CS
+# Creates a new ssh user called $1
+# Adds a public key to that user's authorized keys file
+# Removes root's ability to ssh in
+
+echo "testing 123"
