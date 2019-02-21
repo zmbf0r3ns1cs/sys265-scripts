4624912d49a2d0a708467b2c97cbe50f056109d9
zmbf0r3ns1cs <zburnham781@gmail.com>
2019-02-21 11:37:29 -0500

    Configured secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,4 +4,11 @@
 # Adds a public key to that user's authorized keys file
 # Removes root's ability to ssh in
 
-echo "testing 123"
+sudo useradd $1
+sudo mkdir /home/$1/.ssh
+sudo cp linux/public-keys/sys265.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
+
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
